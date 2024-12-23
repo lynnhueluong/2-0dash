567afac1812d90ebf1db4e9b452737fda6388433
lynnhueluong <lynn@the20.co>
2024-12-23 07:55:41 -0600

    onboarding flow

--- src/app/api/auth/[auth0]/route.ts
+++ src/app/api/auth/[auth0]/route.ts
@@ -1,18 +1,18 @@
 // src/app/api/auth/[auth0]/route.ts
 import { handleAuth, handleLogin } from '@auth0/nextjs-auth0';
 
-const FRAMER_ONBOARDING_URL = 'https://project-dmklsn3yttooaux1sfgg.framercanvas.com/onboarding';
+const FRAMER_PREVIEW_URL = 'https://framerusercontent.com/preview-web/87af2f501b59f4647af60ff68d43fd455e7cff35c5c448db1aa7b0d1f62fa9e9/onboarding';
 
 export const GET = handleAuth({
   signup: handleLogin({
-    returnTo: FRAMER_ONBOARDING_URL,
+    returnTo: FRAMER_PREVIEW_URL,
     authorizationParams: {
       prompt: 'signup',
       screen_hint: 'signup',
     }
   }),
   login: handleLogin({
-    returnTo: FRAMER_ONBOARDING_URL,
+    returnTo: FRAMER_PREVIEW_URL,
     authorizationParams: {
       prompt: 'login',
     }
