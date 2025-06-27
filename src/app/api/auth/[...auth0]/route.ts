5052de417bdf742f3ccf6bdd38166ef06fe81abc
lynnhueluong <lynn@the20.co>
2025-06-27 15:04:50 -0500

    Fix Auth0 redirect to onboarding page

--- src/app/api/auth/[...auth0]/route.ts
+++ src/app/api/auth/[...auth0]/route.ts
@@ -1,6 +1,8 @@
 //src/app/api/auth/[...auth0]/route.ts
 import { handleAuth } from '@auth0/nextjs-auth0';
 
-export const GET = handleAuth();
+export const GET = handleAuth({
+  returnTo: '/onboarding'
+});
 
 export const dynamic = 'force-dynamic';
--- src/app/providers.tsx
+++ src/app/providers.tsx
@@ -1,11 +1,7 @@
 'use client';
 
-import { UserProvider } from '@auth0/nextjs-auth0/client';
+import { UserProvider } from '@auth0/nextjs-auth0';
 
-export function Auth0Provider({ children }: { children: React.ReactNode }) {
-  return (
-    <UserProvider>
-      {children}
-    </UserProvider>
-  );
+export default function Providers({ children }: { children: React.ReactNode }) {
+  return <UserProvider>{children}</UserProvider>;
 }
